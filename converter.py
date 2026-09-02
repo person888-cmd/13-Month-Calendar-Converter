bbe3f30014a30459e76adde0ad2e14b7c9721eb2
person888-cmd <artistbrig@gmail.com>
2026-09-02 15:51:41 -0600

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,19 +1,38 @@
 #Responses are organized by category 
 responses = {
 
-import time
-import datetime
-
-print "Time in seconds since the epoch: %s" %time.time()
-print "Current date and time: ", datetime.datetime.now()
-print "Or like this: ", datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
-
-print "Current year: ", datetime.date.today().strftime("%Y")
-print "Month of year: ", datetime.date.today().strftime("%B")
-print "Week number of the year: ", datetime.date.today().strftime("%W")
-print "Weekday of the week: ", datetime.date.today().strftime("%w")
-print "Day of year: ", datetime.date.today().strftime("%j")
-print "Day of the month : ", datetime.date.today().strftime("%d")
-print "Day of week: ", datetime.date.today().strftime("%A")
-
+"Months":{
+  "January 1" or "January 1st" or "Jan 1" : "January 1st, First day of the year, First day of the First Month",
+  "January 2" or "January 2nd" or "Jan 2" : "January 2nd, Second day of the year, Second day of the First Month",
+  "January 3" or "January 3rd" or "Jan 3" : "January 3rd, Third day of the year, Third day of the First Month",
+  "January 4" or "January 4th" or "Jan 4" : "January 4th, Fourth day of the year, Fourth day of the First Month",
+  "January 5" or "January 5th" or "Jan 5" : "January 5th, Fifth day of the year, Fifth day of the First Month",
+  "January 6" or "January 6th" or "Jan 6" : "January 6th, Sixth day of the year, Sixth day of the First Month",
+  "January 7" or "January 7th" or "Jan 7" : "January 7th, Seventh day of the year, Seventh day of the First Month",
+  "January 8" or "January 8th" or "Jan 8" : "January 8th, Eighth day of the year, Eighth day of the First Month",
+  "January 9" or "January 9th" or "Jan 9" : "January 9th, Nineth day of the year, Nineth day of the First Month",
+  "January 10" or "January 10th" or "Jan 10" : "January 10th, Tenth day of the year, Tenth day of the First Month",
+  "January 11" or "January 11th" or "Jan 11" : "January 11th, Eleventh day of the year, Eleventh day of the First Month",
+  "January 12" or "January 12th" or "Jan 12" : "January 12th, Twelth day of the year, Rwelth day of the First Month",
+  "January 13" or "January 13th" or "Jan 13" : "January 13th, Thirteenth day of the year, Fhirteenth day of the First Month",
+  "January 14" or "January 14th" or "Jan 14" : "January 14th, Fourteenth day of the year, Fourteenth day of the First Month",
+  "January 15" or "January 15th" or "Jan 15" : "January 15th, Fifteenth day of the year, Fifteenth day of the First Month",
+  "January 16" or "January 16th" or "Jan 16" : "January 16th, Sixteenth day of the year, Sixteenth day of the First Month",
+  "January 17" or "January 17th" or "Jan 17" : "January 17th, Seventeenth day of the year, Seventeenth day of the First Month",
+  "January 18" or "January 18th" or "Jan 18" : "January 18th, Eighteenth day of the year, Eighteenth day of the First Month",
+  "January 19" or "January 19th" or "Jan 19" : "January 19th, Nineteenth day of the year, Nineteenth day of the First Month",
+  "January 20" or "January 20th" or "Jan 20" : "January 20th, Twentith day of the year, Twentith day of the First Month",
+  "January 21" or "January 21st" or "Jan 21" : "January 21st, Twenty First day of the year, Twenty First day of the First Month",
+  "January 22" or "January 22nd" or "Jan 22" : "January 22nd, Twenty Second day of the year, Twenty Second day of the First Month",
+  "January 23" or "January 23rd" or "Jan 23" : "January 23nd, Twenty Third day of the year, Twenty Third day of the First Month",
+  "January 24" or "January 24th" or "Jan 24" : "January 24nd, Twenty Fourth day of the year, Twenty Fourth day of the First Month",
+  "January 25" or "January 25th" or "Jan 25" : "January 25th, Twenty Fifth day of the year, Twenty Fifth day of the First Month",
+  "January 26" or "January 26th" or "Jan 26" : "January 26th, Twenty Sixth day of the year, Twenty Sixth day of the First Month",
+  "January 27" or "January 27th" or "Jan 27" : "January 27th, Twenty Seventh day of the year, Twenty Seventh day of the First Month",
+  "January 28" or "January 28th" or "Jan 28" : "January 28th, Twenty Eighth day of the year, Twenty Eighth day of the First Month",
+#Febuary
+  "January 29" or "January 29th" or "Jan 29" : "Febuary 1st, Twenty Nineth day of the year, First day of the Second Month",
+  "January 30" or "January 30th" or "Jan 31" : "Febuary 2nd, Thirtith day of the year, Second day of the Second Month",
+  "January 31" or "January 31st" or "Jan 31" : "Febuary 3rd, Thirty First day of the year, Third day of the Second Month",
+}
 }
